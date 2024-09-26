c1af7846816fcc68b0d9944b2765fcfb9729cb58
toastedbreadandomelette <toastedbreadandomelette@gmail.com>
2024-09-26 22:11:28 +0530

    Minor config changes

--- build.js
+++ build.js
@@ -2,20 +2,15 @@
 
 import esbuild from "esbuild";
 
-async function watch() {
-    let context = await esbuild.context({
+async function build() {
+    esbuild.build({
         logLevel: "info",
         entryPoints: ["./src/index.ts", './src/scripts/phase-vocoder.service.js'],
         bundle: true,
         outdir: './dist',
+        minify: true
     });
-    await context.watch().then(async _ => {
-        await context.serve({ port: 4200, servedir: './dist' }).then(data => {
-            console.log(data);
-        });
-    });
-    console.log('Watching over the process yall...');
 }
 
 console.log(process.cwd())
-watch()
+build()
--- run.js
+++ run.js
@@ -2,15 +2,20 @@
 
 import esbuild from "esbuild";
 
-async function build() {
-    esbuild.build({
+async function watch() {
+    let context = await esbuild.context({
         logLevel: "info",
         entryPoints: ["./src/index.ts", './src/scripts/phase-vocoder.service.js'],
         bundle: true,
-        outdir: './dist',
-        minify: true
+        outdir: './dev',
     });
+    await context.watch().then(async _ => {
+        await context.serve({ port: 4200, servedir: './dev' }).then(data => {
+            console.log(data);
+        });
+    });
+    console.log('Watching over the process yall...');
 }
 
 console.log(process.cwd())
-build()
+watch()
