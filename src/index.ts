d141386aba061ba01283fd280ce97ccc0f73cae7
toastedbreadandomelette <toastedbreadandomelette@gmail.com>
2025-07-20 21:45:01 +0530

    Reset index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,92 +11,92 @@ inject();
 
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 function theme() {
-    if (
-        localStorage.theme === 'dark' || (
-            !('theme' in localStorage) &&
-            window.matchMedia &&
-            window?.matchMedia('(prefers-color-scheme: dark)').matches
-        )
-    ) {
-        document.documentElement.classList.add('dark')
-        localStorage.theme = 'dark'
-    }
+  if (
+    localStorage.theme === 'dark' || (
+      !('theme' in localStorage) &&
+      window.matchMedia &&
+      window?.matchMedia('(prefers-color-scheme: dark)').matches
+    )
+  ) {
     document.documentElement.classList.add('dark')
     localStorage.theme = 'dark'
+  }
+  document.documentElement.classList.add('dark')
+  localStorage.theme = 'dark'
 }
 
 window.onload = async () => {
-    theme();
-    const wasmFile = await fetch('./wasm_fft_bg.wasm');
-    const wasmBin = await wasmFile.arrayBuffer();
+  theme();
+  const wasmFile = await fetch('./wasm_fft_bg.wasm');
+  const wasmBin = await wasmFile.arrayBuffer();
 
-    var subscriber = new Subscriber();
-    subscriber.createSubscription('onbodyload');
+  var subscriber = new Subscriber();
+  subscriber.createSubscription('onbodyload');
 
-    var globalAudioService = new GlobalAudioService(subscriber, wasmBin);
+  var globalAudioService = new GlobalAudioService(subscriber, wasmBin);
 
-    let canvasThemeSelector = new CanvasThemeSelectorView(subscriber);
-    let window2 = new WindowView(globalAudioService, subscriber);
-    let panelView = new PanelView(subscriber);
-    panelView.attachViews(window2.getAllAttachableViews());
-    panelView.attachViews(canvasThemeSelector.getAllAttachableViews());
+  let canvasThemeSelector = new CanvasThemeSelectorView(subscriber);
+  let window2 = new WindowView(globalAudioService, subscriber);
+  let panelView = new PanelView(subscriber);
+  panelView.attachViews(window2.getAllAttachableViews());
+  panelView.attachViews(canvasThemeSelector.getAllAttachableViews());
 
-    const dropHandler = (event: DragEvent) => {
-        event.preventDefault();
+  const dropHandler = (event: DragEvent) => {
+    event.preventDefault();
 
-        if (event.dataTransfer?.files) {
-            const file = event.dataTransfer.files[0];
-            window2.setSourceFile(file);
-        }
-    };
-    const onDragOver = (event: DragEvent) => {
-        event.preventDefault();
-    };
-
-    const setResize = (_: Event) => {
-        window2.setResize();
+    if (event.dataTransfer?.files) {
+      const file = event.dataTransfer.files[0];
+      window2.setSourceFile(file);
     }
+  };
+  const onDragOver = (event: DragEvent) => {
+    event.preventDefault();
+  };
 
-    const content = el('div')
-        .mcls('main-content')
-        .inner([window2.getView(), panelView.getView()])
-        .evt('drop', dropHandler)
-        .evt('dragover', onDragOver)
-        .get();
+  const setResize = (_: Event) => {
+    window2.setResize();
+  }
 
-    const player = (evt: KeyboardEvent) => {
-        switch (evt.code) {
-            case 'Space':
-                evt.preventDefault();
-                evt.stopPropagation();
-                window2.onPlayerPausedOrResumed();
-                break;
-        }
+  const content = el('div')
+    .mcls('main-content')
+    .inner([window2.getView(), panelView.getView()])
+    .evt('drop', dropHandler)
+    .evt('dragover', onDragOver)
+    .get();
+
+  const player = (evt: KeyboardEvent) => {
+    switch (evt.code) {
+      case 'Space':
+        evt.preventDefault();
+        evt.stopPropagation();
+        window2.onPlayerPausedOrResumed();
+        break;
     }
+  }
 
-    const keyUp = (evt: KeyboardEvent) => {
-        switch (evt.code) {
-            case 'ControlLeft':
-                el(panelView.getView()).tcls('hidden');
-                break;
-        }
+  const keyUp = (evt: KeyboardEvent) => {
+    switch (evt.code) {
+      case 'ControlLeft':
+        el(panelView.getView()).tcls('hidden');
+        break;
     }
+  }
 
-    window.addEventListener('resize',  (evt) => setResize(evt));
+  window.addEventListener('resize',  (evt) => setResize(evt));
 
-    el(document.body)
-        .mcls('w-avail', 'h-avail')
-        .evt('keydown', player)
-        .evt('keyup', keyUp)
-        .evt('dragover', onDragOver)
-        .evt('drop', dropHandler)
-        .evt('resize', setResize)
-        .inner([
-            el('div')
-                .mcls('application', 'flex', 'flex-col')
-                .inners(content)
-                .get()
-        ]);
+  el(document.body)
+    .mcls('w-avail', 'h-avail')
+    .evt('keydown', player)
+    .evt('keyup', keyUp)
+    .evt('dragover', onDragOver)
+    .evt('drop', dropHandler)
+    .evt('resize', setResize)
+    .inner([
+      el('div')
+        .mcls('application', 'flex', 'flex-col')
+        .inners(content)
+        .get()
+    ]);
 
-    subscriber.fire<void>('onbodyload');
+  subscriber.fire<void>('onbodyload');
 }
